099c054bfef1a17a864f2578204001ab97dd89b4
Aniket <aniket.amin@bacancytechnology.com>
2019-10-20 20:35:25 +0530

    initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+    export default {
+        // methods: {
+        //     increment() {
+        //         this.$store.commit('increment')
+        //     },
+        //     decrement() {
+        //         this.$store.commit('decrement')
+        //     }
+        // }
+
+        methods : {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ])
+        }
+    }
+</script>
---
+++ src/components/Result.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>clicks : {{ clicks }} </p>
+        <p>clicks : {{ clicks }} </p>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+    export default {
+        computed : {
+            ...mapGetters([
+                'doubleCounter',
+                'clicks'
+            ]),
+            clickss(){
+                return this.$store.state.counter
+            }
+        }
+            // counter(){
+            //     return this.$store.getters.doubleCounter;
+            // },
+            // clicks(){
+            //     return this.$store.getters.clicks;
+            // }
+        
+    }
+</script>
